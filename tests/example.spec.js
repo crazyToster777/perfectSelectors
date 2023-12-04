6a13fdc0f2506e6176c6aee58610a8103d9d2676
Daniil Druzhinin <dany@wisebits.com>
2023-12-04 19:54:19 +0200

    Add playwright

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,12 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
+test('Fill form', async ({page}) => {
+    await page.goto('https://demoqa.com/automation-practice-form');
+    await page.locator('[class="col-md-4 col-sm-6"]:nth-of-type(2) input').fill('test')
+    await page.locator('html > body > div:nth-of-type(2) > div > div > div:nth-of-type(2) > div:nth-of-type(2) > div:nth-of-type(2) > form > div:nth-of-type(1) > div:nth-of-type(4) > input').fill('test2');
+    await page.locator('input[class="mr-sm-2 form-control"]').fill('test@email.com');
+    await page.locator('//div[@class="custom-control custom-radio custom-control-inline"]/*[@id="gender-radio-2"]').check();
+    await page.locator('input[maxlength="10"]').fill('1234567');
+    await page.waitForTimeout(1000);
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
